27e8def03c9493541c94cdc689e4b925904deb4b
gco <gco@jazzhaiku.com>
2023-03-13 23:16:31 -0700

    Update dev_ops with quiesce

--- tun.c
+++ tun.c
@@ -142,7 +142,12 @@ static	struct dev_ops tun_ops = {
   nodev,		/* devo_reset */
   &tun_cb_ops,		/* devo_cb_ops */
   (struct bus_ops *)NULL,/* devo_bus_ops */
-  NULL		         /* devo_power */
+#if DEVO_REV < 4
+  NULL			/* devo_power */
+#else
+  NULL,			/* devo_power */
+  ddi_quiesce_not_needed /* devo_quiesce */
+#endif
 };
 
 static struct modldrv modldrv = {
